0f8c0554ba42754b404277f9bed7fd731dcae5c8
Dale Wickizer <daleewickizer@gmail.com>
2014-08-15 22:39:59 -0400

    Modified and tested cacheMatrix.R for Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,87 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix(x)
+
+## Receives an n x n invertible matrix
+## Caches functions for the matrix and its inverse
+
+## The 'set' function sets the value of the matrix
+## The 'get' function gets the value of the matrix
+## The 'setInverse' uses 'solve' to set inverse of the matrix
+## The 'getInverse' gets the inverse of the matrix
+
+## NOTE: for this assignment all input matrices are assumed to be invertible
+##       There is not error checking for invertibility.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {                       ## Create set(y) function in next environment
+        x <<- y                                ## It will be used to store the inverse on the stack
+        m <<- NULL                             ## 'm' will be used as a semaphore to determine 
+    }                                          ## whether the inverse has been calculated or not
+    
+    get <- function() x                        ## The get() function copies the orig 'x' on stack
+    
+    setInverse <- function(solve) m <<- solve  ## setInverse() uses solve(), stores inverse in 'm'
+                                               ## and pushes it on the stack
+    
+    getInverse <- function() m                 ## getInverse() pops a copy of inverse off the stack
+    list(set = set, get = get,                 ## A list of these functions is returnted to the 
+         setInverse = setInverse,              ## calling environment
+         getInverse = getInverse)
 }
 
+## Function cacheSolve(x, ...)
 
-## Write a short comment describing this function
+## Accepts the list (stack) of functions from makeCacheMatrix and the original input matrix
+## It checks to see if the inverse has already been solved (using the solve function).
+## If it has already been solved, it pulls the result from cache
+## If it hasn't, it runs solve to find the inverse.
+## The returned matrix is the inverse of 'x' that was originally given to makeCacheMatrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+    m <- x$getInverse()                 ## Pop the inverse of 'x' from the stack, it it exists
+    if(!is.null(m)) {                   ## Check to see if the inverse was already solved, if so...
+        message("getting cached data")  ## Notify that result was found in cache
+        return(m)                       ## Return cached result
+    }
+    data <- x$get()                     ## If result is not in cache, pop a copy of x from the stack
+    m <- solve(data, ...)               ## Solve for the inverse
+    x$setInverse(m)                     ## Push a copy on the stack to cache for next iteration
+    
+    m                                   ## Return uncached inverse of 'x'
 }
+
+
+## Function testCacheSolve() to test makeCacheMatrix and cacheSolve
+## Executes without arguments
+##
+
+testCacheSolve <- function() {
+    
+    testMatrix <- rbind(c(1,3,5),c(0,1,0),c(7:9))
+    
+    a <- makeCacheMatrix(testMatrix)
+    message("After makeCacheMatrix, resulting list")
+    
+    print(a)
+    
+    for (i in 1:3) {
+        b <- cacheSolve(a)
+        
+        message("Calculated inverse of x:")
+        print(b)
+        
+    }
+    
+    message("2nd and 3rd Passes above should have been cached\n")
+    
+    print
+    
+    message("Verifying that result is truly inverse...")
+    message("Should see identity matrix:")
+    
+    testMatrix %*% b
+    
+}
+
